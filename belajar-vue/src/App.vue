b4586897ab932e9ec5aabb56016b38f2c6f9134b
IKBALmiftahudn <ikbalmiiftahudin222>
2023-07-21 15:58:43 +0700

    commit 1

--- belajar-vue/src/App.vue
+++ belajar-vue/src/App.vue
@@ -1,25 +1,85 @@
-<script setup>
-import { ref } from 'vue'
- 
-//no rawHtml
-const msg ="<h1>Hello World</h1>"
-//rawHtml n(menggunakan html yang ada dalam variable )
-const rawHtml = " <h1> <font color = 'blue'> Hello World </font> </h1>"
-// v-bind (menggunakan atribut html ke vue.js)
-const link = "https://github.com/"
-//aritmatika
-const number= prompt("")
-</script>
-
 <template>
-  <div id="app">
-    <span>{{ msg }}</span><br>
-    <p><span v-html="rawHtml"></span></p><br>
-    <a :href="link">Github</a><br>
-    <p>{{ parseInt(number) }}</p>
-    <p>{{ number ? 'TRUE : 1' : 'FALSE : 0' }}</p>
-    <br>
-    <button :disabled="isButtonDisabled">Button</button>
-  
+  <div>
+  <h2>Formulir</h2>
+  <form @submit.prevent="submitForm">
+      <label for="">Nama</label>
+      <input  type="text" name="nama" v-model="siswa.nama">
+      <br>
+      <label for="">Kelas</label>
+      <input  type="text" name="kelas" v-model="siswa.kelas">
+      <br>
+      <label for="">Alamat</label>
+      <input type="textarea" name="alamat" v-model="siswa.alamat">
+      <br>
+      <label for="">Nilai Matematika</label>
+      <input type="text" name="mtk" v-model="siswa.mtk">
+      <br>
+      <label for="">NIlai B.Indonesia</label>
+      <input type="text" name="indo" v-model="siswa.indo">
+      <br>
+      <label for="">Nilai IPA</label>
+      <input type="text" name="ipa" v-model="siswa.ipa">
+      <br>
+      <button type="submit">Simpan</button>
+    </form>
+    <div v-if="showResult">
+      nama : {{ name }} <br>
+      kelas : {{ kelas }} <br>
+      alamat : {{ alamat }} <br>
+      Nilai Matematika : {{ mtk }} <br>
+      Nilai Indonesia : {{ indo }} <br>
+      Nilai IPA : {{ ipa }} <br>
+      Total : {{ total }} <br>
+      Keterangan : {{ keterangan }}
+    </div>
   </div>
 </template>
+
+<script>
+import { ref, toDisplayString } from 'vue';
+
+export default {
+  name  : 'Mycomponent',
+  data() {
+    return {
+      siswa: {
+        nama: '',
+        kelas: '',
+        alamat: '',
+        mtk:'',
+        indo:'',
+        ipa:'',
+      },
+      
+      showResult: ref(false),
+      name: ref(''),
+      kelas: ref(''),
+      alamat: ref(''),
+      mtk: ref(''),
+      indo: ref(''),
+      ipa: ref(''),
+      total : 0 ,
+      keterangan : ref('')
+    };
+  },
+ 
+ 
+  methods: {
+    submitForm() {
+      this.name = this.siswa.nama;
+      this.kelas = this.siswa.kelas;
+      this.alamat = this.siswa.alamat;
+      this.mtk = this.siswa.mtk;
+      this.indo = this.siswa.indo;
+      this.ipa = this.siswa.ipa;
+      this.total = parseInt(this.mtk) + parseInt(this.indo) + parseInt(this.ipa);
+      if (this.total >240){
+        this.keterangan ="Lulus"
+      }else{
+        this.keterangan ="Tidak Lulus"
+      }
+      this.showResult = true;
+    },
+  },
+};
+</script>
