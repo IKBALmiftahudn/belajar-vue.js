6f385b224ba39f94e3a582a43bf32b3e49ff2092
IKBALmiftahudn <ikbalmiiftahudin222>
2023-07-26 15:20:23 +0700

    myproject

---
+++ myproject/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <navbarComponent></navbarComponent>
+    <headerComponent></headerComponent>
+    <contentComponent></contentComponent>
+    <br>
+    <br>
+    <footerComponent></footerComponent>
+  </div>
+
+  </template>
+
+  <script setup>
+import navbarComponent from './components/navbarComponent.vue';
+import headerComponent from './components/headerComponent.vue'
+import contentComponent from './components/contentComponent.vue'
+import footerComponent from './components/footerComponent.vue'
+</script>
